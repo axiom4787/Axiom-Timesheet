3fe86b0e861ec9f21e203a9458d2541e6e4c43fc
Tanmay <mail2tanmaygarg@gmail.com>
2024-12-19 17:01:40 -0600

    quick documentation

--- sheets_input.py
+++ sheets_input.py
@@ -8,7 +8,13 @@ for person in data:
     names.append(person[0])
 
 
-def checkin(name, date, time):
+def checkin(name: str, date: str, time: str):
+    """
+    Checks in a student into the timesheet log and updates it in both local list and gsheet.
+    :param name: name of the student
+    :param date: the date of the entry
+    :param time: the time of the entry
+    """
     print(f"Checking in {name}")
     data.append([name, date, time, '', ''])
     row = data.index([name, date, time, '', '']) + 1
@@ -16,6 +22,12 @@ def checkin(name, date, time):
 
 
 def update_total_time(index, final_time):
+    """
+    calculates the total time a student has spent and updates it in both local list and gsheet.
+    :param index: the index of the entry that is being checked out
+    :param final_time: time_out
+    :return: the difference in time-out and time-in in terms of hours in decimals
+    """
     time1 = datetime.datetime.strptime(data[index][2], '%H:%M:%S.%f')
     time2 = datetime.datetime.strptime(final_time, '%H:%M:%S.%f')
     time_difference = (time2 - time1).total_seconds()
@@ -25,12 +37,22 @@ def update_total_time(index, final_time):
 
 
 def checkout(name, time, row):
+    """
+    checks out a student and updates the local list and gsheet accordingly.
+    :param name: the student that is checking out
+    :param time: the time when they checked out
+    :param row: index at which the entry is in the list
+    """
     print(f"Checking out {name}")
     data[row] = [name, data[row][1], data[row][2], time, update_total_time(row, time)]
     sheet.sheet1.update(f"A{row+1}:E{row+1}", [data[row]])
 
 
 def add_time(name):
+    """
+    adds the entry itself to the local list and the gsheet.
+    :param name: the student that is being logged
+    """
     time = str(datetime.datetime.now().time())
     date = str(datetime.datetime.now().date())
 
