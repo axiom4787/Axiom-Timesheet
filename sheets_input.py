39980d9b09a2eb76a6452858139b79aced00c7e5
Tanmay <mail2tanmaygarg@gmail.com>
2024-12-19 11:26:51 -0600

    fixed total time

--- sheets_input.py
+++ sheets_input.py
@@ -15,19 +15,19 @@ def checkin(name, date, time):
     sheet.sheet1.update(f"A{row}:C{row}", [[name, date, time]])
 
 
-def update_total_time(index):
-    time_format = "%:%:%"
-    time1 = datetime.datetime.strptime(data[index][2], time_format)
-    time2 = datetime.datetime.strptime(data[index][3], time_format)
-    time_difference = time2 - time1
+def update_total_time(index, final_time):
+    time1 = datetime.datetime.strptime(data[index][2], '%H:%M:%S.%f')
+    time2 = datetime.datetime.strptime(final_time, '%H:%M:%S.%f')
+    time_difference = (time2 - time1).total_seconds()
+    time_difference = round(time_difference / 3600, 2)
     data[index][3] = time_difference
     return time_difference
 
 
 def checkout(name, time, row):
     print(f"Checking out {name}")
-    data[row-1] = [name, data[row-1][1], data[row-1][2], time, update_total_time(row-1)]
-    sheet.sheet1.update(f"A{row+1}:E{row+1}", [data[row-1]])
+    data[row] = [name, data[row][1], data[row][2], time, update_total_time(row, time)]
+    sheet.sheet1.update(f"A{row+1}:E{row+1}", [data[row]])
 
 
 def add_time(name):
