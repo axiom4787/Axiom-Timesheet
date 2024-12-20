36650911fe54a100ca31247c73a8636626f8b33a
Tanmay <mail2tanmaygarg@gmail.com>
2024-12-20 13:24:28 -0600

    running time locally
    should theoretically work

--- running_time.py
+++ running_time.py
@@ -2,7 +2,12 @@ from connection import sheet
 from log_output import data as entries
 
 running_time_data = sheet.sheet3.get_all_values()
+running_time_data.pop(0)
+names = [time[0] for time in running_time_data]
+running_time = {}
 
+for name in names:
+    running_time[name] = 0
 
 for entry in entries:
-    print(entry)
+    running_time[entry[0]] += entry[4]
