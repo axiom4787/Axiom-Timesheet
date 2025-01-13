9873aef405a3a980fd1296f244ec464019b7e876
Angelo <102323862+agaur12@users.noreply.github.com>
2025-01-13 16:41:25 -0600

    Added unconnected settings menu w/ load funcions

--- connection.py
+++ connection.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import datetime
+import settings
+import warnings
 
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -10,5 +12,10 @@ scopes = [
 cred = Credentials.from_service_account_file("credentials.json", scopes=scopes)
 client = gspread.authorize(cred)
 
-sheets_id = "1rzZ0NeR_ETB4F2hoxuZZb2ELTq36oKg0j3UZ3MTb-3Y"
+try:
+    sheets_id = settings.load("sheets_id")
+except FileNotFoundError:
+    warnings.warn("SHEET ID NOT FOUND. USING DEFAULT SHEET ID")
+    sheets_id = "1rzZ0NeR_ETB4F2hoxuZZb2ELTq36oKg0j3UZ3MTb-3Y"
+
 sheet = client.open_by_key(sheets_id)
