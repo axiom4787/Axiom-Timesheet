cd0106988b29629e273584a039d59fb96e239b84
Tanmay <mail2tanmaygarg@gmail.com>
2025-01-30 22:06:08 -0600

    initial quick commit

--- connection.py
+++ connection.py
@@ -1,6 +1,20 @@
 import gspread
 from google.oauth2.service_account import Credentials
-import datetime
+import pypyodbc as odbc
+
+DRIVER_NAME = "ODBC Driver 18 for SQL Server"
+SERVER_NAME = "I46061L52295744\AXIOMSQL"
+DATABASE_NAME = "TestTimesheet"
+
+connection = f"""
+    DRIVER={{{DRIVER_NAME}}};
+    SERVER={SERVER_NAME};
+    DATABASE={DATABASE_NAME};
+    Trust_Connection=yes;
+"""
+
+conn = odbc.connect(connection)
+print(conn)
 
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
